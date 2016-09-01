ed75ef420aa8e0c2f1a49d0f7518220b72bf6328
Mattressburn <chevyboxer@gmail.com>
2016-09-01 12:14:32 -0400

    initial push

---
+++ index.js
@@ -0,0 +1,81 @@
+var QUESTIONS = [
+    {
+        text: '<:48:x<:65:=<:6C:$=$=$$~<:03:+$~<:ffffffffffffffbd:+$<:ffffffffffffffb1:+$<:57:~$~<:18:x+$~<:03:+$~<:06:x-$x<:0e:x-$=x<:43:x-$',
+        answers: [
+            '0815',
+            '2B',
+            'BAM128',
+            'Barely'
+        ],
+        correct: 0
+    },
+    {
+        text: '+0+0+0+0+0+0+0+2)+0+0+9)+7))+3)-0-0-0-0-0-0-0-9)+0+0+0+0+0+0+0+0+7)-8)+3)-6)-8)-7-0-0-0-0-0-0)',
+        answers: [
+            '0815',
+            '2B',
+            'BAM128',
+            'Barely'
+        ],
+        correct: 1
+    },
+    {
+        text: '*6*3p*4*3*2*0p*2*1*0pp>0*1*0p*5*4*0p*5*4*2*1*0p*4*3p*1*0p/+0p+0*6*5*2p+0*5*0p',
+        answers: [
+            '0815',
+            '2B',
+            'BAM128',
+            'Barely'
+        ],
+        correct: 2
+    },
+    {
+        text: ']xhhhhooooooooohhhhhhxooooooooxooooooxjjjxhoooohhhxhohhhhhhhxhhhhjjjhhhxhhhhooooooooohhhhhhxjjjxxjjjjjjjxjhhhhxjhhhhhhhhjjjhh~',
+        answers: [
+            '0815',
+            '2B',
+            'BAM128',
+            'Barely'
+        ],
+        correct: 3
+    }
+];
+
+
+
+var state = {
+  currentquestion: 0,
+  answeredCorrectly: []
+};
+
+var nextQuestion = function(state) {
+  state.currentquestion++;
+};
+
+var userSubmittedAnswer = function(state,answer) {
+  if (answer) {
+    state.answeredCorrectly.push(state.currentquestion)
+  }
+}
+
+var getScore = function(state) {
+  return state.answeredCorrectly.length
+}
+
+var resetQuiz = function(state) {
+  state.currentquestion = 0
+  state.answeredCorrectly = []
+}
+
+
+renderQuestionText(state, $('.question'));
+
+
+// Fake user scenario:
+console.log("Initial state: ");
+console.dir(state);
+userSubmittedAnswer(state, true);
+nextQuestion(state);
+console.log("After user answered question 1 correctly:");
+console.log(getScore(state))
+console.dir(state);
